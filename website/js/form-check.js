26a14e85204ae6a1442435ec378c3584156c0aa9
Federico Rapetti <fuji1097@gmail.com>
2015-07-22 02:46:13 +0200

    Fix/Add username and password registration fields

--- website/js/form-check.js
+++ website/js/form-check.js
@@ -5,16 +5,14 @@ $(document).ready(function() {
 	var pass_min_chars = 6;
 
 	//result texts  
-	var characters_error = 'Il numero minimo di caratteri è ';  
+	var characters_error = 'Il numero minimo di caratteri Ã¨ ';  
 	var checking_html = 'Controllo...';  
 
 	//when button is clicked  
-	$( "#username-new" ).change(function() {
-		console.log("Checking Password");
-		alert("Checking password");
+	$( '#txt-username' ).change(function() {
 		//run the character number check
-		$('#username-status').css("display","inline-block");
-		if($('#username-new').val().length < user_min_chars){  
+		//$('#username-status').css("display","inline-block");
+		if($('#txt-username').val().length < user_min_chars){  
 			//if it's bellow the minimum show characters_error text '  
 			$('#username-status').html(characters_error + user_min_chars);  
 			$('#username-status').css("background-color","#D32F2F");
@@ -23,14 +21,13 @@ $(document).ready(function() {
 			$('#username-status').html(checking_html);  
 			check_username_availability();  
 		}
+		$('#username-status').fadeIn("400");
 	});
 	
-	$( "#password" ).change(function() {
-		console.log("Checking Password");
-		alert("Checking password");
+	$( "#txt-password" ).change(function() {
 		//run the character number check
-		$('#password-status').css("display","block");
-		if($('#password').val().length < pass_min_chars){  
+		//$('#password-status').css("display","block");
+		if($(this).val().length < pass_min_chars){  
 			//if it's bellow the minimum show characters_error text '
 			$('#password-status').css("background-color","#D32F2F");
 			$('#password-status').html(characters_error + pass_min_chars);  
@@ -39,27 +36,42 @@ $(document).ready(function() {
 			$('#password-status').html('Password valida');
 			$('#password-status').css("background-color","#4CAF50");
 		}
-	   });
+		$('#password-status').fadeIn("400");
 	});
 	
+	$( "#txt-re-password" ).change(function() {
+		//run the character number check
+		//$('#password-status').css("display","block");
+		if($(this).val() != $("#txt-password").val()){  
+			//if it's bellow the minimum show characters_error text '
+			$('#re-password-status').css("background-color","#D32F2F");
+			$('#re-password-status').html("Le password non sono identiche");  
+		} else {  
+			//ok
+			$('#re-password-status').css("background-color","#4CAF50");
+			$('#re-password-status').html('Password identica');
+		}
+		$('#re-password-status').fadeIn("400");
+	});
+});
 	
 
 //function to check username availability  
 function check_username_availability(){  
 
 	//get the username  
-	var username = $('#username').val();  
+	var username = $('#txt-username').val();  
 
 	//use ajax to run the check  
 	$.post("include/check_exist.php", { username: username, type: "username" }, function(result) {  
 		//if the result is 1  
 		if(result == 1){  
 			//show that the username is available  
-			$('#username-status').html(username + ' è disponibile');
+			$('#username-status').html(username + ' Ã¨ disponibile');
 			$('#username-status').css("background-color","#4CAF50");
 		}else{  
 			//show that the username is NOT available  
-			$('#username-status').html(username + ' non è disponibile');
+			$('#username-status').html(username + ' non Ã¨ disponibile');
 			$('#username-status').css("background-color","#D32F2F");
 		}  
 	});  
--- website/signup.php
+++ website/signup.php
@@ -20,34 +20,36 @@
 						
 						<form action="signup.php" method="post">
 							<table class="table-spaced">
-								<tr>
-									<td colspan="2">
+								<tr class="bottom-aligned">
+									<td>
 										<label for="username">Username:</label><br>
-										<input id="username-new" type="text" name="username" required>
+										<input type="text" name="username" id="txt-username">
+									</td>
+									<td>
 										<div id="username-status"></div>
 									</td>
 								</tr>
-								<tr>
+								<tr class="top-aligned">
 									<td>
 										<label for="password">Password:</label><br>
-										<input id="password" type="password" name="password" require>
+										<input type="password" name="password" id="txt-password">
 										<div id="password-status"></div>
 									</td>
 									<td>
 										<label for="re-password">Reinserisci la Password:</label><br>
-										<input type="password" name="re-password" autocomplete="off"><br>
+										<input type="password" name="re-password" id="txt-re-password" autocomplete="off"><br>
 										<div id="re-password-status"></div>
 									</td>
 								</tr>
-								<tr>
+								<tr class="top-aligned">
 									<td>
 										<label for="email">Email:</label><br>
-										<input type="email" name="email" required><br>
+										<input type="email" name="email" id="txt-email"><br>
 										<div id="email-status"></div>
 									</td>
 									<td>
 										<label for="re-email">Reinserisci l'Email:</label><br>
-										<input type="email" name="re-email" autocomplete="off" require><br>
+										<input type="email" name="re-email" id="txt-re-email" autocomplete="off"><br>
 										<div id="re-email-status"></div>
 									</td>
 								</tr>
